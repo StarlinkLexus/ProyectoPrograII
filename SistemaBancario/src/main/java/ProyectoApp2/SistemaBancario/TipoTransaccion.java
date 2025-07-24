90cc6cf483a93f475c8f7e9451726acb1eba077b
StarlinkLexus <ale_vallejo16@outlook.es>
2025-07-23 21:54:30 -0500

    Tipos de transaccion

---
+++ SistemaBancario/src/main/java/ProyectoApp2/SistemaBancario/TipoTransaccion.java
@@ -0,0 +1,10 @@
+package ProyectoApp2.SistemaBancario;
+
+/*
+ * 
+ * Enums que representan los tipos de transacciones que se pueden hacer
+ */
+public enum TipoTransaccion {
+	Retiro, Deposito
+
+}
